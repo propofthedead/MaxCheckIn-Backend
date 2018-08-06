53a56ebd172e1accf8d68d1ce0b00914c26f6ce7
Jonathan Wilson <jon.wilsond@gmail.com>
2018-08-06 10:22:59 -0400

    Added class class

--- ClassSignIn_Hackathon_BE/Models/Class.cs
+++ ClassSignIn_Hackathon_BE/Models/Class.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace ClassSignIn_Hackathon_BE.Models {
 	public class Class {
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		[Required]
+		public string Location  { get; set; }
+		[Required]
+		public bool Active { get; set; } = true;
+
+		public List<Student> Students { get; set; }
+
+		public Class() { 
 	}
 }
--- ClassSignIn_Hackathon_BE/Models/LogDbContext.cs
+++ ClassSignIn_Hackathon_BE/Models/LogDbContext.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace ClassSignIn_Hackathon_BE.Models {
-	public class Class1 {
+	public class LogDbContext: DbContext {
+		//public DbSet<User> Users { get; set; }
+		//public DbSet<Vendor> Vendors { get; set; }
+		//public DbSet<Product> Products { get; set; }
+		//public DbSet<PurchaseRequest> PurchaseRequests { get; set; }
+		//public DbSet<PurchaseRequestLineItem> PurchaseRequestLineItems { get; set; }
+
+		public LogDbContext() : base() { }
+		}
 	}
-}

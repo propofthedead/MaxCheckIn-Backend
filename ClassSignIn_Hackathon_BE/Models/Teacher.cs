ed50615fd4e2326625178a2924f64efc1b2e2a8e
Foster Stule <camodude5@yahoo.com>
2018-08-19 00:35:26 -0400

    added a teachers model

---
+++ ClassSignIn_Hackathon_BE/Models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClassSignIn_Hackathon_BE.Models
+{
+	public class Teacher
+	{
+		[Required]
+		public int Id { get; set; }
+		[Required]
+		public string Firstname { get; set; }
+		[Required]
+		public string Lastname { get; set; }
+		[Required]
+		public int Pin { get; set; }
+		[Required]
+		public virtual List<Class> Classes { get; set; }
+
+	}
+}
